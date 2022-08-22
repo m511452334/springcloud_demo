b8155dc28de0fb6ef126a570568bbb7fcc12327a
lcm <511452334@qq.com>
2022-08-23 01:27:46 +0800

    first commit

---
+++ cloud-demo/eureka-server/src/main/java/cn/itcast/EurekaApplication.java
@@ -0,0 +1,11 @@
+package cn.itcast;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lichengming
+ * @date 2022/8/23 -1:14
+ */
+@SpringBootApplication
+public class EurekaApplication {
+}
