ab109c4b7eb22dbaa369910a19b4881f2abfb50a
m511452334 <m511452334@qq.com>
2022-08-31 00:35:09 +0800

    springclould commit

--- cloud-demo/order-service/src/main/java/cn/itcast/order/mapper/OrderMapper.java
+++ cloud-demo/order-service/src/main/java/cn/itcast/order/mapper/OrderMapper.java
@@ -1,10 +1,8 @@
 package cn.itcast.order.mapper;
 
 import cn.itcast.order.pojo.Order;
-import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
-@Mapper
 public interface OrderMapper {
 
     @Select("select * from tb_order where id = #{id}")
